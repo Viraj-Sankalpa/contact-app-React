1c4e7dc35bcf372d745c206d287472d1c0b84260
Viraj-Sankalpa <virajsankalpa@gmail.com>
2023-07-05 15:20:02 +0530

    Store contacts to local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 // import React from "react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddContact from './AddContact';
 import "./App.css";
 import ContactList from "./ContactList";
@@ -7,11 +7,17 @@ import Header from "./Header";
 
 function App() {
 
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const addContactHandler = (contact) =>{
     console.log(contact);
+    setContacts([...contacts,contact]);
   }
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="ui container">
